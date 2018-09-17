7cf1dd8d59e4e146c56440b69c91e181bf0bf50b
Timo Runge <me@timorunge.com>
2018-09-17 16:22:11 +0200

    * Better Ansible linting (for tests)

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,10 +3,20 @@ set -e
 
 test -z ${override_variable} && echo "Missing environment variable: override_variable" && exit 1
 
-printf "[defaults]\nroles_path=/etc/ansible/roles\n" > ansible.cfg
+printf "[defaults]\nroles_path=/etc/ansible/roles\n" > /ansible/ansible.cfg
 
-ansible-lint /ansible/test.yml
-ansible-lint /etc/ansible/roles/${ansible_role}/tasks/main.yml
+if [ ! -f /etc/ansible/lint.zip ]; then
+  wget https://github.com/ansible/galaxy-lint-rules/archive/master.zip -O \
+  /etc/ansible/lint.zip
+  unzip /etc/ansible/lint.zip -d /etc/ansible/lint
+fi
+
+ansible-lint -c /etc/ansible/roles/${ansible_role}/.ansible-lint -r \
+  /etc/ansible/lint/galaxy-lint-rules-master/rules \
+  /etc/ansible/roles/${ansible_role}
+ansible-lint -c /etc/ansible/roles/${ansible_role}/.ansible-lint -r \
+  /etc/ansible/lint/galaxy-lint-rules-master/rules \
+  /ansible/test.yml
 
 ansible-playbook /ansible/test.yml \
   -i /ansible/inventory \
